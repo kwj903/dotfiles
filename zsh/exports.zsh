6773760771bcc6bae9119e26ffe8c9a7a740a908
kwj903 <kwakwoojae@gmail.com>
2026-03-06 20:27:09 +0900

    initial dotfiles setup

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,23 @@
+typeset -U path PATH
+
+export PYENV_ROOT="$HOME/.pyenv"
+export NVM_DIR="$HOME/.nvm"
+
+export EDITOR="code --wait"
+export VISUAL="$EDITOR"
+export PAGER="less -FRX"
+
 path=(
   "$HOME/.local/bin"
-  "/Applications/Docker.app/Contents/Resources/bin"
+  "/usr/local/bin"
+  "/usr/local/sbin"
+  "/usr/bin"
+  "/bin"
+  "/usr/sbin"
+  "/sbin"
   $path
 )
+
+[[ -d "$PYENV_ROOT/bin" ]] && path=("$PYENV_ROOT/bin" "$PYENV_ROOT/shims" $path)
 
 export PATH
--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -1,8 +1,37 @@
+# fzf
 export FZF_CTRL_T_OPTS="--preview 'bat --color=always {}' --preview-window=right:60%"
+export _ZO_FZF_OPTS="--height=40% --reverse"
 
-export _ZO_FZF_OPTS='--height=40% --reverse'
+if [[ -f "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh" ]]; then
+  source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
+fi
 
-[[ -f "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh" ]] && \
-source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
+if [[ -f "$(brew --prefix)/opt/fzf/shell/completion.zsh" ]]; then
+  source "$(brew --prefix)/opt/fzf/shell/completion.zsh"
+fi
 
-eval "$(direnv hook zsh)"
+# direnv
+if command -v direnv >/dev/null 2>&1; then
+  eval "$(direnv hook zsh)"
+fi
+
+# pyenv
+if command -v pyenv >/dev/null 2>&1; then
+  eval "$(pyenv init - zsh)"
+fi
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+
+if [[ -s "/usr/local/opt/nvm/nvm.sh" ]]; then
+  source "/usr/local/opt/nvm/nvm.sh"
+fi
+
+if [[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ]]; then
+  source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+fi
+
+# zoxide
+if command -v zoxide >/dev/null 2>&1; then
+  eval "$(zoxide init zsh)"
+fi
